9ade70d0ae1f8ac410348837e20c07672cc357d9
Theodora Georgescu <georgescutheodora20@stud.ase.ro>
2022-11-28 19:57:04 +0200

    first commit

---
+++ Project_Ticketing/Project_Ticketing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project_Ticketing
+//
+//  Created by Theodora Georgescu on 28.11.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
